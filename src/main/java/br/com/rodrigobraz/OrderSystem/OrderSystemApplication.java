35a705b42b6a3b8199081c5b5bc5cc73ee8b2c98
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-01-18 18:46:15 -0300

    Project created

---
+++ src/main/java/br/com/rodrigobraz/OrderSystem/OrderSystemApplication.java
@@ -0,0 +1,15 @@
+package br.com.rodrigobraz.OrderSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderSystemApplication.class, args);
+
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/br/com/rodrigobraz/OrderSystem/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.OrderSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
