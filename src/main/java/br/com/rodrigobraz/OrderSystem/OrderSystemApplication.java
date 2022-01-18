4d1029f34dc6880938428df6fec068c32d19089a
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-01-18 20:00:45 -0300

    Instatiating operation created

--- src/main/java/br/com/rodrigobraz/OrderSystem/OrderSystemApplication.java
+++ src/main/java/br/com/rodrigobraz/OrderSystem/OrderSystemApplication.java
@@ -1,13 +1,33 @@
 package br.com.rodrigobraz.OrderSystem;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.rodrigobraz.OrderSystem.domain.Category;
+import br.com.rodrigobraz.OrderSystem.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class OrderSystemApplication {
+public class OrderSystemApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderSystemApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informatica");
+		Category cat2 = new Category(null, "Escritorio");
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
